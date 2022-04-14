fb660e5b2056cea2cd048b0297a09d8fcf832dd1
Aniket1675 <Aniket@05>
2022-04-14 14:15:04 +0530

    ADDED:feature to calculate the fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("welcome to cab invoice generator program");
+    private final int COST_PER_KM = 10;
+    private final int COST_PER_MINUTE = 1;
+    public int MIN_FARE = 5;
+    double totalFare;
+
+    public double CalculateFare(double DISTANCE, int TIME) {
+
+        totalFare = COST_PER_KM * DISTANCE + COST_PER_MINUTE * TIME;
+        if(totalFare < MIN_FARE)
+            totalFare = MIN_FARE;
+        return totalFare;
     }
 }
